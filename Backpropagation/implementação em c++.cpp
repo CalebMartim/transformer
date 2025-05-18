7dac642823aae30c4e5f59dd04a868c72ea8d6d6
Caleb Martim <calebmartim2113@gmail.com>
2025-05-18 20:15:30 -0300

    Backward pass funcionando

--- Backpropagation/implementação em c++.cpp
+++ Backpropagation/implementação em c++.cpp
@@ -1,35 +1,37 @@
-// Vamos fazer um value que implementa backpropagation 
+// Vamos fazer um value que implementa backpropagation [x] 
 // Vamos construir um neuron
 // Vamos construir um layer
 // Vamos construir uma MLP
 
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
 using ld = long double;
 
 struct value; 
-vector<value*> values;
+vector<value> values;
+vector<ld> grad;
 
 struct value{
-  ld data, grad;
-  value *left_child, *right_child;  
-  string op;
+  ld data;
+  int left_child, right_child;  
+  int op;
   int id; 
   ld expoente;
 
-  value(ld data = 0, value *left_child = nullptr, value *right_child = nullptr, string op = "", ld expoente = 0){
-    this->data = data;
-    this->grad = 0;
-    this->left_child = left_child;
-    this->right_child = right_child;
-    this->op = op;
-    this->id = size(values);
-    this->expoente = expoente;
-    values.push_back(this);
+  value(ld _data = 0, int _left_child = -1, int _right_child = -1, int _op = 0, ld _expoente = 0) {
+    this->data = _data;
+    this->left_child = _left_child;
+    this->right_child = _right_child;
+    this->op = _op;
+    this->id = (int) size(values);
+    this->expoente = _expoente;
+    // Faz a cópia deste valor
+    values.push_back(*this); 
+    grad.push_back(0);
   }
 
   // Mostra em algum outputstream
@@ -39,23 +41,25 @@ struct value{
   }
 
   value operator+(value b) {
-    return value(this->data + b.data, this, values[b.id], "+");
+    return value(data + b.data, id, b.id, 1);
   }
   
   value operator+(ld b) {
-    return value(this->data + b, this, nullptr, "+");
+    value x(b);
+    return (*this) + x;
   }
 
   friend value operator+(ld x, value a) {
-    return value(a.data + x, &a, nullptr, "+");
+    return a + x;
   }
 
   value operator*(value b) {
-    return value(this->data * b.data, this, values[b.id], "*");
+    return value(data * b.data, id, b.id, 2);
   }
 
   value operator*(ld b) {
-    return value(this->data * b, this, nullptr, "*");
+    value x(b);
+    return (*this) * x;
   }
 
   friend value operator*(ld b, value a) {
@@ -75,90 +79,95 @@ struct value{
   }
 
   value pow(ld k) {
-    return value(powl(this->data, k), this, nullptr, "pow", k);
+    return value(powl(data, k), id, -1, 3, k);
   }
 
   value operator/(value b) {
-    return (*this) * b.pow(-1);
+   return (*this) * b.pow(-1);
+  }
+
+  value operator/(ld b) {
+   return (*this) * (1 / b);
   }
 
   friend value operator/(ld b, value a) {
-    return a.pow(-1) * b;
+   return b * a.pow(-1);
   }
 
   value exp() {
-    return value(powl(2.718281828459045, this->data), this, nullptr, "exp");
+    return value(powl(2.718281828459045, data), id, -1, 4);
   }
 
   void prop() {
-    string op = this->op;
-    if (op == "+") {
-      this->left_child->grad += this->grad;
-      this->right_child->grad += this->grad;
-    } else if (op == "*") {
-      this->left_child->grad += this->right_child->data * this->grad;
-      this->right_child->grad += this->left_child->data * this->grad;
-    } else if (op == "exp") {
-      this->left_child->grad += this->left_child->data * this->grad;
-    } else if (op == "pow") {
-      this->left_child->grad += this->expoente * powl(this->left_child->data, this->expoente - 1) * this->grad;
+    if (op == 1) {
+      grad[left_child] += grad[id];
+      grad[right_child] += grad[id];
+    } else if (op == 2) {
+      grad[left_child] += values[right_child].data * grad[id];
+      grad[right_child] += values[left_child].data * grad[id];
+    } else if (op == 3) {
+      grad[left_child] += expoente * powl(values[left_child].data, expoente - 1) * grad[id];
+    } else if (op == 4) {
+      grad[left_child] += data * grad[id];
     }
   }
 
   void backward_pass(){
-    this->grad = 1;
-    this->prop();
+    grad[id] = 1;
+    
+    // Vamos fazer aqui a ordenção topológica:
+    vector<int> top_sort;
+    vector<bool> vis(size(values));
+    auto dfs = [&](int v, auto &&self) -> void {
+      if (v == -1 or vis[v]) return;
+      vis[v] = true;
+      if(values[v].left_child) {
+        self(values[v].left_child, self);
+        if (values[v].right_child) {
+          self(values[v].right_child, self);
+        }
+      }
+      top_sort.push_back(v);
+    };
+    
+    dfs(id, dfs);
+    reverse(top_sort.begin(), top_sort.end());
+
+    for (int v : top_sort) {
+      values[v].prop();
+    }
   }
 };
 
 int main(){
-  value a(10.0);
-  value b(2.0);
-  value d(1.5);
-  value c = b + ((2 + (((b + a) + b))) + a) * d;
+  cout << "Teste 1:\n";
 
-  // cout << size(values) << '\n';
-  // for (int i = 0; i < size(values); ++i) {
-  //   cout << *values[i] << '\n';
-  // }
+  value x1(2.0);
+  value x2(0.0);
 
-  queue<value*> bfs;
-  vector<bool> vis(size(values));
+  value w1(-3.0);
+  value w2(1.0);
 
-  bfs.push(&c);
-  while (bfs.empty() == false) {
-    value &x = *bfs.front(); 
-    bfs.pop();
-    if (vis[x.id]) continue;
-    vis[x.id] = true;
-    cout << x << ' ' << x.op << '\n';
-    if (x.left_child != nullptr) {
-      bfs.push(x.left_child);
-      if (x.right_child != nullptr) {
-        bfs.push(x.right_child);
-      }
-    }
-  } 
+  value b(6.8813735870195432);
 
-  // cout << "Mudança de valores\n";
-  // cout << *values[0] << '\n';
-  // a.data = 3;
-  // cout << *values[0] << '\n';
+  value n = x1*w1 + x2*w2 + b;
 
-  cout << '\n' << "Teste de gradiente:\n";
-
-  value x(3.0);
-  value y(5.0);
-  cout << "x: " << x << '\n' << "y: " <<  y << '\n';
-  value z = x * y;
-  cout << "Resultado " << z << '\n';
-
-  z.backward_pass();
-
-  cout << "Gradientes:\n";
-  cout << "x: " << x.grad << '\n';
-  cout << "y: " << y.grad << '\n';
-  cout << "z: " << z.grad << '\n';
-
+  value e = (2*n).exp();
   
+  value o = (e - 1) / (e + 1);
+
+  o.backward_pass();
+  
+  cout << grad[w1.id] << ' ' << grad[w2.id] << ' ' << grad[b.id] << '\n';
+
+  cout << "Teste 2:\n";
+  value teste1(-2);
+  value teste2(3);
+  value c = teste1 + teste2;
+  value d = teste1 * teste2;
+  value E = c * d;
+  
+  E.backward_pass();
+
+  cout << grad[teste1.id] << ' ' << grad[teste2.id] << '\n';
 }
